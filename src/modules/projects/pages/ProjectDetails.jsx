fb7e8e547f28af781ccdc2418777f6b808fbde25
Akilesh S <akileshselva909@gmail.com>
2026-07-08 08:23:54 +0530

    Fixed : Product detail page Not loading

--- src/modules/projects/pages/ProjectDetails.jsx
+++ src/modules/projects/pages/ProjectDetails.jsx
@@ -14,6 +14,7 @@ import { useToast } from '../../../shared/context/ToastContext';
 import { useAuth } from '../../auth/context/AuthContext';
 import { ProgressiveAuthModal } from '../../auth/components/ProgressiveAuthModal';
 import { SEO, PageType, generateSEO } from '../../../shared/seo';
+import { eventTracker } from '../../../shared/analytics/index.js';
 
 // Custom SVG Icons
 const DIYIcon = () => (
